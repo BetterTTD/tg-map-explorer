3f0592b91e891ea37e9a359abdb0a13a421d7b55
Sergey Konoplich <s.konopl@yandex.ru>
2021-07-04 21:14:06 +0300

    BitOpes module

---
+++ src/BitOps.elm
@@ -0,0 +1,5 @@
+module BitOps exposing (getBits)
+
+import Bitwise exposing (shiftRightBy, shiftLeftBy)
+getBits: Int -> Int -> Int -> Int
+getBits offset size source = Bitwise.and (shiftRightBy source offset) (shiftLeftBy size 1)
